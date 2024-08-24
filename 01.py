9c56178886dd11579001bfceecdf12e8811b4e43
Tanisha020 <tanisha2318@gmail.com>
2024-08-24 11:54:00 +0530

    added basic lesson

---
+++ 01.py
@@ -0,0 +1,33 @@
+# Question - 0
+# create a variable called color with an appropriate value on the line below
+color = 'blue'
+
+# Question - 1
+pi = 3.14159 # approximate
+diameter = 3
+# Create a variable called 'radius' equal to half the diameter
+radius = diameter/2
+# Create a variable called 'area', using the formula for the area of a circle: pi times the radius squared
+area = pi * radius ** 2
+
+# Question - 2
+a = [1, 2, 3]
+b = [3, 2, 1]
+# Your code goes here. Swap the values to which a and b refer.
+a,b = b,a
+
+# Question - 3(a)
+# Add parentheses to the following expression so that it evaluates to 1.
+(5 - 3) // 2
+# Question - 3(b)
+# Add parentheses to the following expression so that it evaluates to 0.
+(8 - 3) * (2 - (1 + 1))
+
+# Question - 4
+# Variables representing the number of candies collected by alice, bob, and carol
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+# Your code goes here! Replace the right-hand side of this assignment with an expression
+# involving alice_candies, bob_candies, and carol_candies
+to_smash = (alice_candies + bob_candies + carol_candies) % 3
